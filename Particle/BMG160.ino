31b1bd88880f6bc6d08f4c321e546811c639bc8a
zaphod-42 <trey@tomadoh.com>
2017-09-12 13:48:21 -0500

    Create BMG160.ino

---
+++ Particle/BMG160.ino
@@ -0,0 +1,15 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "BMG160.h"
+
+BMG160 sensor;
+
+void setup() {
+    sensor.init();
+    Particle.variable("gyroX", sensor.gyroX);
+    Particle.variable("gyroY", sensor.gyroX);
+    Particle.variable("gyroZ", sensor.gyroZ);
+}
+
+void loop() {
+    sensor.loop();
+}
